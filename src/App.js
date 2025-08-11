249153d9f3205033f5244c5d8ba7d58487017f1d
Michael-Shvab <84184425+MichaelShvab@users.noreply.github.com>
2025-08-11 11:23:46 +0300

    First Changes
    Changes the GET part/General shiz for now

--- src/App.js
+++ src/App.js
@@ -9,27 +9,35 @@ function App() {
       <Router>
         <Navbar />
         <Sidebar />
+        
         <Switch>
           <Route exact path='/'>
             <Home />
           </Route>
+
           <Route exact path='/about'>
             <About />
           </Route>
+
           <Route exact path='/products'>
             <Products />
           </Route>
+
           <Route exact path='/cart'>
             <Cart />
           </Route>
+
           <Route exact path='/products/:id' children={<SingleProduct />} />
+
           <PrivateRoute exact path='/checkout'>
             <Checkout />
           </PrivateRoute>
+
           <Route path='*'>
             <Error />
           </Route>
         </Switch>
+
         <Footer />
       </Router> 
     </AuthWrapper>
--- src/context/products_context.js
+++ src/context/products_context.js
@@ -42,7 +42,9 @@ export const ProductsProvider = ({ children }) => {
 
     try {
       const response = await axios.get(url)
-      const products = response.data
+      const products = response.data.data
+      console.log(url)
+      
       dispatch({type: GET_PRODUCTS_SUCCESS, payload: products})
     } catch (error) {
       dispatch({type: GET_PRODUCTS_ERROR })
@@ -51,9 +53,11 @@ export const ProductsProvider = ({ children }) => {
 
   const fetchSingleProduct = async (url) => {
     dispatch({type: GET_SINGLE_PRODUCT_BEGIN})
+
     try {
       const response = await axios.get(url)
       const singleProduct = response.data
+      console.log(singleProduct);
       dispatch({type: GET_SINGLE_PRODUCT_SUCCESS, payload: singleProduct})
     } catch(error) {
       dispatch({type: GET_SINGLE_PRODUCT_ERROR})
